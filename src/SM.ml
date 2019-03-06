c1ae347fb3cc0ebf6c14754ab61f3583657e2ed4
Tanya-Gordeyeva <tangord@mail.ru>
2019-03-07 01:46:35 +0300

    hw3

--- src/SM.ml
+++ src/SM.ml
@@ -24,7 +24,23 @@ type config = int list * Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+ let instruct config evt =
+  let (stack, f) = config in
+  let (st, input, output) = f in
+    match evt with
+      | ST var   -> (match stack with
+                      | x::rest -> rest, (Language.Expr.update var x st, input, output))
+      | LD var   -> [st var] @ stack, f
+      | READ     -> (match input with
+                      | x::rest -> [x] @ stack, (st, rest, output))
+      | WRITE    -> (match stack with
+                      | x::rest -> rest, (st, input, output @ [x]))
+      | BINOP operator -> (match stack with
+                      | y::x::rest -> [Language.Expr.operator operator x y] @ rest, f)
+      | CONST x  -> [x] @ stack, f
+
+
+let eval config prog = List.fold_left instruct config prog
 
 (* Top-level evaluation
 
@@ -41,4 +57,14 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+ let rec comp_expr exp = match exp with  
+    | Language.Expr.Var x                   -> [LD x]
+    | Language.Expr.Binop (operator, left, right) -> (comp_expr left) @ (comp_expr right) @ [BINOP operator]
+    | Language.Expr.Const x                 -> [CONST x];;
+
+
+let rec compile program = match program with
+    | Language.Stmt.Write exp               -> (comp_expr exp) @ [WRITE]
+    | Language.Stmt.Seq (f, s)        -> (compile f) @ (compile s)
+    | Language.Stmt.Assign (curr, exp)      -> (comp_expr exp) @ [ST curr]
+    | Language.Stmt.Read c                -> [READ; ST c]
